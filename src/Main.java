926028f8b160fe28c0fe40410b30e968f0124343
mezitest <mezis4u@inbox.ru>
2022-11-30 16:23:28 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int amount = 5329;
+        int miles = 20;
+        int bonus = amount / miles;
+        System.out.println("Начисленные мили = " + bonus);
+    }
+}
