eb9ec8f6ec37ae32613d66715038890f469424fa
mezitest <mezis4u@inbox.ru>
2022-12-04 12:11:01 +0500

    add the class BonusMilesService

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 public class Main {
-
     public static void main(String[] args) {
-        int amount = 5329;
-        int miles = 20;
-        int bonus = amount / miles;
-        System.out.println("Начисленные мили = " + bonus);
+        BonusMilesService service = new BonusMilesService();
+        /* в куске кода из описания дз используется price, но перед этим говорится использовать
+            параметр cost, что, как я поняла, и есть price, поэтому я заменила в части Main price на cost
+         */
+        int cost = 10_000;
+        int miles = service.calculate(cost);
+        System.out.println(miles);
     }
 }
